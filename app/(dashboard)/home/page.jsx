d6f4c154332df0e3140ad42b79a852c792d7638c
diego <diegoeliseoiovane@gmail.com>
2023-10-25 09:49:19 -0300

    deploy last test

--- app/(dashboard)/home/page.jsx
+++ app/(dashboard)/home/page.jsx
@@ -1,109 +1,20 @@
-// import { cookies } from "next/headers"
+import { cookies } from "next/headers"
 import Home from "@/app/_components/dashboard/home/Home"
 import { getSolarmanStationsData } from "@/app/_lib/getSolarmanStationsData"
-// import { getSolarmanCommunityData } from "@/app/_lib/getSolarmanCommunityData"
-// import { getSolarmanUserData } from "@/app/_lib/getSolarmanUserData"
+import { getSolarmanCommunityData } from "@/app/_lib/getSolarmanCommunityData"
+import { getSolarmanUserData } from "@/app/_lib/getSolarmanUserData"
 
 const HomePage = async () => {
 
-  let response
-  let one
-  let two
-  let three
+  const token = cookies().get("token")
+  const solarmanStationsData =  await getSolarmanStationsData()
+  const communityData = getSolarmanCommunityData(solarmanStationsData)
+  const userData = token ? await getSolarmanUserData(token, solarmanStationsData) : null
 
-  let solarmanStationsData
-
-  const appId = process.env.SOLARMAN_APP_ID
-  const userPassword = process.env.SOLARMAN_PASSWORD
-  const SHA256 = require("crypto-js/sha256")
-  const hashedPassword = SHA256(userPassword).toString()
-
-  try {
-
-    const accessTokenResponse = await fetch(`https://globalapi.solarmanpv.com/account/v1.0/token?appId=${appId}&language=en`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        password: hashedPassword,
-        email: process.env.SOLARMAN_USER, // SOLARMAN BUSINESS USER
-        appSecret: process.env.SOLARMAN_APP_SECRET, // SOLARMAN BUSINESS APP SECRET 
-      }),
-    })
-
-    const { access_token } = await accessTokenResponse.json()
-
-    one = access_token
-
-    // // GET THE ORGANIZATION ID - ORG IS SOLBOX
-    // const companyIdResponse = await fetch(`https://globalapi.solarmanpv.com/account/v1.0/info?language=en`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Authorization: `Bearer ${access_token}`,
-    //   },
-    // })
-
-    // const companyData = await companyIdResponse.json()
-
-    // two = companyData
-
-    // // API REQUIRES TO GET A SECOND TOKEN RELATED TO THE ORGANIZATION ID - IN THIS CASE ONLY FOR SOLBOX BUSINESS
-    // const orgAccessTokenResponse = await fetch(`https://globalapi.solarmanpv.com/account/v1.0/token?appId=${appId}&language=en`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     password: hashedPassword,
-    //     email: process.env.SOLARMAN_USER, // SOLARMAN BUSINESS USER
-    //     appSecret: process.env.SOLARMAN_APP_SECRET, // SOLARMAN BUSINESS APP SECRET 
-    //     orgId: companyData.orgInfoList[0].companyId, // ID COMES FROM LAST FETCH
-    //   }),
-    // })
-
-    // const orgAccessData = await orgAccessTokenResponse.json()
-
-    // three = orgAccessData
-
-    // // GET STATIONS/PLANTS FOR THIS ORGANIZATION (SOLBOX - BUSINESS)
-    // const stationsResponse = await fetch(
-    //   "https://globalapi.solarmanpv.com/station/v1.0/list?language=en",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       page: 1, // MAX 200 p/page
-    //       size: 200,
-    //     }),
-    //     headers: {
-    //       Authorization: `Bearer ${orgAccessData.access_token}`,
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // )
-
-    // solarmanStationsData = await stationsResponse.json()
-
-    //   response = solarmanStationsData
-
-  } catch (err) {
-
-    console.log("ERROR....", err)
-    response = err
-  }
-
-  // const token = cookies().get("token")
-  // const solarmanStationsData =  await getSolarmanStationsData()
-  // const communityData = getSolarmanCommunityData(solarmanStationsData)
-  // const userData = token ? await getSolarmanUserData(token, solarmanStationsData) : null
-
-  // console.log("User Data.,......", userData)
-  console.log("hola")
+  console.log(solarmanStationsData)
 
   return (
-    // <Home communityData={communityData} userData={userData} />
-    <Home data={response} one={one} />
+    <Home communityData={communityData} userData={userData} />
   )
 }
 
--- app/_components/dashboard/home/Home.jsx
+++ app/_components/dashboard/home/Home.jsx
@@ -1,9 +1,9 @@
 "use client"
 
-// import { lazy, Suspense } from "react"
-// import CommunityCharts from './communityCharts/CommunityCharts'
-// import UserPlants from "./userPlants/UserPlants"
-// import UserCharts from "./userCharts/UserCharts"
+import { lazy, Suspense } from "react"
+import CommunityCharts from './communityCharts/CommunityCharts'
+import UserPlants from "./userPlants/UserPlants"
+import UserCharts from "./userCharts/UserCharts"
 import {
   Container,
   Header,
@@ -19,70 +19,61 @@ import {
   MapContainer,
 } from './Elements'
 
-// const Map = lazy(() => import("../map/Map"))
+const Map = lazy(() => import("../map/Map"))
 
-const Home = ({ data, one }) => {
-  // const Home = ({ communityData, userData }) => {
-
-  // console.log("Data", data)
-  // const x = Object.values(data)
-  // console.log("Values...", x)
-
-  console.log("HERE........", one)
-
-  // console.log(userData.userTotalPower)
+const Home = ({ communityData, userData }) => {
 
   return (
     <Container>
-      Pruebaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      <Header>
+        <WelcomeText>Bienvenido a la comunidad Solbox</WelcomeText>
+      </Header>
+      {
+        userData ?
+        <>
+          <Section>
+            <Column>
+              <ColumnTitle>Mis plantas de producción</ColumnTitle>
+              <ProductionBox>
+                <ProductionNumber>{userData.userTotalPower}<span>kWh</span></ProductionNumber>
+                <ProductionText>Total energía producida</ProductionText>
+                <ProductionDetail>Desde el 17-08-23</ProductionDetail>
+              </ProductionBox>
+              <UserPlants data={userData.userPlants} />
+            </Column>
+            <Column>
+              <ColumnTitle>Ahorros equivalentes a:</ColumnTitle>
+              <UserCharts totalIndividualProduction={userData.userTotalPower} />
+            </Column>
+            <Column></Column>
+          </Section>
+          <Hr />
+        </> : null
+      }
+      <Section>
+        <Column>
+          <ColumnTitle>Comunidad Solbox</ColumnTitle>
+          <ProductionBox>
+            <ProductionNumber>{communityData.plantsTotalEnergy}<span>kWh</span></ProductionNumber>
+            <ProductionText>Total energía producida</ProductionText>
+            <ProductionDetail>Desde el 17-08-23</ProductionDetail>
+          </ProductionBox>
+          <ColumnTitle>Nuestros paneles</ColumnTitle>
+          <MapContainer>
+              <Map plantsLocations={communityData.plantsLocations} />
+          </MapContainer>
+        </Column>
+        <Column>
+          <ColumnTitle>Ahorros equivalentes a:</ColumnTitle>
+          <CommunityCharts plantsTotalEnergy={communityData.plantsTotalEnergy} />
+        </Column>
+      </Section>
+      <Section id="mobile-dashboard-sidebar">
+        <Column>
+          {/* <DashboardSidebar /> */}
+        </Column>
+      </Section>
     </Container>
-    // <Container>
-    //   <Header>
-    //     <WelcomeText>Bienvenido a la comunidad Solbox</WelcomeText>
-    //   </Header>
-    //   {
-    //     userData ?
-    //     <>
-    //       <Section>
-    //         <Column>
-    //           <ColumnTitle>Mis plantas de producción</ColumnTitle>
-    //           <ProductionBox>
-    //             <ProductionNumber>{userData.userTotalPower}<span>kWh</span></ProductionNumber>
-    //             <ProductionText>Total energía producida</ProductionText>
-    //             <ProductionDetail>Desde el 17-08-23</ProductionDetail>
-    //           </ProductionBox>
-    //           <UserPlants data={userData.userPlants} />
-    //         </Column>
-    //         <Column>
-    //           <ColumnTitle>Ahorros equivalentes a:</ColumnTitle>
-    //           <UserCharts totalIndividualProduction={userData.userTotalPower} />
-    //         </Column>
-    //         <Column></Column>
-    //       </Section>
-    //       <Hr />
-    //     </> : null
-    //   }
-    //   <Section>
-    //     <Column>
-    //       <ColumnTitle>Comunidad Solbox</ColumnTitle>
-    //       <ProductionBox>
-    //         <ProductionNumber>{communityData.plantsTotalEnergy}<span>kWh</span></ProductionNumber>
-    //         <ProductionText>Total energía producida</ProductionText>
-    //         <ProductionDetail>Desde el 17-08-23</ProductionDetail>
-    //       </ProductionBox>
-    //       <ColumnTitle>Nuestros paneles</ColumnTitle>
-    //       <MapContainer>
-    //           <Map plantsLocations={communityData.plantsLocations} />
-    //       </MapContainer>
-    //     </Column>
-    //     <Column>
-    //       <ColumnTitle>Ahorros equivalentes a:</ColumnTitle>
-    //       <CommunityCharts plantsTotalEnergy={communityData.plantsTotalEnergy} />
-    //     </Column>
-    //   </Section>
-    //   <Section id="mobile-dashboard-sidebar">
-    //   </Section>
-    // </Container>
   )
 }
 
