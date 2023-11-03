246d76ada3ab4f281588b31f2525288a7ba6d81c
diego <diegoeliseoiovane@gmail.com>
2023-11-03 14:35:46 -0300

    correct layout

--- app/(dashboard)/config/page.jsx
+++
@@ -1,5 +0,0 @@
-import Config from "@/app/_components/dashboard/config/Config"
-
-const ConfigPage = async () => <Config />
-
-export default ConfigPage
--- app/_components/dashboard/layout/nav/Nav.jsx
+++ app/_components/dashboard/layout/nav/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = ({ logged }) => {
                 <Logo src="https://firebasestorage.googleapis.com/v0/b/solbox-app.appspot.com/o/solbox-logo.png?alt=media&token=7f8dbf38-1da3-443d-bd53-ff6eccff66ff&_gl=1*hux6co*_ga*MjA5NzcwMTI5NS4xNjk1NzM4Mzcw*_ga_CW55HF8NVT*MTY5Njk1Mjc1OS4yLjEuMTY5Njk1MzcxMi41MC4wLjA." alt="" fill sizes="10vw" priority />
             </LogoContainer>
             <MenuIcon open={open} setOpen={setOpen} />
-            { open ? <DynMobileNav logged={logged} setOpen={setOpen}/> : null }
+            {open ? <DynMobileNav logged={logged} setOpen={setOpen} /> : null}
             <Navigation>
                 <NavLink href="/home" >
                     <span><MdOutlineDashboardCustomize /></span>
@@ -48,18 +48,12 @@ const Nav = ({ logged }) => {
                             <span><BiLogInCircle /></span>
                             Ingresar
                         </NavLink> :
-                        <>
-                            <NavLink href="/config" >
-                                <span><RiSettingsLine /></span>
-                                Configuración
-                            </NavLink>
-                            <form>
-                                <FakeNavLink formAction={logout}>
-                                    <span><BiLogOutCircle /></span>
-                                    Salir
-                                </FakeNavLink>
-                            </form>
-                        </>
+                        <form>
+                            <FakeNavLink formAction={logout}>
+                                <span><BiLogOutCircle /></span>
+                                Salir
+                            </FakeNavLink>
+                        </form>
                 }
                 <NavLink href="/contact" >
                     <span><GrSend /></span>
